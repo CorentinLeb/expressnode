25d14326692b66186f6c26d83e6ce5984ef938bc
Sébastien Avenet <sebastien.avenet1@gmail.com>
2021-02-26 14:34:20 +0100

    fixed Port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors=require('cors');
 const app=express();
-const PORT = process.env.POST ||3000
+const PORT = process.env.PORT ||3000
 const mongoose = require('mongoose')
 require('dotenv/config');
 
